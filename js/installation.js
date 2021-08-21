509db12d8c5e24708fb6a6ed314ca96641b54f99
Max Marquis <maxim.marquis@gmail.com>
2021-08-21 10:46:59 -0400

    v3

--- js/installation.js
+++ js/installation.js
@@ -24,9 +24,22 @@ function saveBeforeInstallPromptEvent(evt) {
  */
 function installPWA(evt) {
     // CODELAB: Add code show install prompt & hide the install button.
+    deferredInstallPrompt.prompt();
+    // Hide the install button, it can't be called twice.
+    installButton.setAttribute("hidden", true);
+
     // CODELAB: Log user response to prompt.
+    deferredInstallPrompt.userChoice.then((choice) => {
+        if (choice.outcome === "accepted") {
+            console.log("User accepted the A2HS prompt", choice);
+        } else {
+            console.log("User dismissed the A2HS prompt", choice);
+        }
+        deferredInstallPrompt = null;
+    });
 }
-// CODELAB: Add event listener for appinstalled event
+// Add event listener for appinstalled event
+window.addEventListener("appinstalled", logAppInstalled);
 /**
  * Event handler for appinstalled event.
  * Log the installation to analytics or save the event somehow.
@@ -34,5 +47,6 @@ function installPWA(evt) {
  * @param {Event} evt
  */
 function logAppInstalled(evt) {
-    // CODELAB: Add code to log the event
+    // Add code to log the event
+    console.log("Weather App was installed.", evt);
 }
