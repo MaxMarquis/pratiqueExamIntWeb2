07d04b44dfa1c064f9ce68b239fb24c0a0cb5ae8
Max Marquis <maxim.marquis@gmail.com>
2021-08-21 09:43:43 -0400

    offline

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v2.1";
 
 //Add list of files to cache here.
-const FILES_TO_CACHE = ["index.html"];
+const FILES_TO_CACHE = ["index.html", "offline.html"];
 
 self.addEventListener("install", (evt) => {
     console.log("[ServiceWorker] Install");
@@ -36,4 +36,15 @@ self.addEventListener("activate", (evt) => {
 self.addEventListener("fetch", (evt) => {
     console.log("[ServiceWorker] Fetch", evt.request.url);
     //Add fetch event handler here.
+    if (evt.request.mode !== "navigate") {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(() => {
+            return caches.open(CACHE_NAME).then((cache) => {
+                return cache.match("pratiqueExamIntWeb2/offline.html");
+            });
+        })
+    );
 });
